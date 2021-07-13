f8dac9926170e5c5bd1f029ee694f71001d8a298
Modeste Gougbedji <modeste.gougbedji@luxdatadigit.lu>
2021-07-13 18:00:05 +0100

    updates

---
+++ ylomi-prod/dist/js/chunk-2d237ca6.0d9cad7f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d237ca6"],{fd2d:function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v(" Footer ")])},o=[],u={components:{}},l=u,r=t("2877"),s=Object(r["a"])(l,c,o,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d237ca6.0d9cad7f.js.map
